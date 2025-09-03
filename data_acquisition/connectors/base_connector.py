2d2dfbfa255d2d53f675a9e4028408b759e46e5f
cfischa <96009533+cfischa@users.noreply.github.com>
2025-09-03 23:00:56 +0200

    clean up

--- data_acquisition/connectors/base_connector.py
+++
@@ -1,8 +0,0 @@
-# Inside base_connector.py
-class BaseConnector:
-    def __init__(self, source_name):
-        self.source_name = source_name
-    
-    def fetch_data(self):
-        """Must be implemented by subclasses"""
-        raise NotImplementedError
